7a1a1ec2ebbd47e91bb4f82ca61cabf652f4b31a
zaynex <279648970@qq.com>
2016-10-17 15:56:56 +0800

    finsh photo

--- src/js/main.js
+++ src/js/main.js
@@ -5,4 +5,19 @@ $(document).ready(function(){
 		autoPlay: true,
 		stopOnHover: true
 	});
+
+	function makeImageResponsive(){
+		var width = $(window).width();
+		var img = $('.content img');
+		if(width <= 480){
+			img.attr('src', 'img/480.png');
+		}else if(width <= 480){
+			img.attr("src", 'img/800.png');
+		}else {
+			img.attr('src', 'img/1600.png');
+		}
+	}
+
+	$(window).on('resize load', makeImageResponsive);
+	//通过cookie进行设置也可以
 });
