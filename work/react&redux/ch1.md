9d59a33e15b4ad5241a24e29a97d990ad3cd03d4
zaynex <zaynex@126.com>
2016-11-24 23:03:44 +0800

    add 11.24

---
+++ work/react&redux/ch1.md
@@ -0,0 +1,15 @@
+（1）Web 应用是一个状态机，视图与状态是一一对应的。
+（2）所有的状态，保存在一个对象里面。
+
+Store 就是保存数据的地方，你可以把它看成一个容器。整个应用只能有一个 Store。
+
+
+注意：上面的这个 “更新” 并不是指 reducer 可以直接对 state 进行修改
+Redux 规定，须先复制一份 state，在副本 nextState 上进行修改操作
+例如，可以使用 lodash 的 cloneDeep，也可以使用 Object.assign / map / filter/ ... 等返回副本的函数
+
+通过每个reducer函数来决定每个action(type里面就是类型方法)如何改变应用的state状态。(state都是以一个对象树的形式存储在单一的store中)
+
+
+单一数据源
+整个应用的 state 被储存在一棵 object tree 中，并且这个 object tree 只存在于唯一一个 store 中。
---
+++ work/react&redux/test.js
@@ -0,0 +1,79 @@
+Store 就是保存数据的地方，你可以把它看成一个容器。整个应用只能有一个 Store。
+
+import {createStore} from 'redux'
+const store = createStore(fn);
+//接受另外一个函数作为参数，生成新的store对象
+
+
+Store对象包含所有数据。如果想得到某个时点的数据，就要对 Store 生成快照。
+这种时点的数据集合，就叫做 State。
+
+const state = store.getState();
+
+Redux 规定， 一个 State 对应一个 View。
+
+Action 描述当前发生的事情。改变 State 的唯一办法，就是使用 Action。它会运送数据到 Store。
+const action = {
+	type: 'ADD_TODO',
+	payload: 'Learn Redux'
+}
+
+Action Creator
+
+
+const ADD_TODO = '添加 TODO';
+
+function addTodo(text){
+	return {
+		type: ADD_TODO,
+		text
+	}
+}
+
+// addTodo函数就是一个 Action Creator。
+const action = addTodo('Learn Redux');
+
+
+// store.dispatch()是 View 发出 Action 的唯一方法。
+import { createStore } from 'redux';
+const store = createStore(fn);
+store.dispath({
+	type: 'ADD_TODO',
+	payload: 'Learn Redux'
+});
+// 上面代码中，store.dispatch接受一个 Action 对象作为参数，将它发送出去。
+
+
+store.dispath(addTodo('ean'))
+
+
+function reducer(state, action){
+	return Object.assign({}, state, {thingTochange});
+}
+
+function reducer(state, action){
+	return [...state, newItem];
+}
+
+
+let unsubscribe = store.subscribe(() => {console.log(store.getState())});
+
+unsubscribe();
+
+
+var initState = {
+	counter: 0,
+	todos: []
+}
+
+function reducer(state, action){
+	if(!state) state = initState;
+	switch(action.type) {
+		case 'ADD_TODO':
+			var nextState = _.cloneDeep(state)
+			nextState.todos.push(action.payload)
+			return nextState
+
+		deg
+	}
+}
