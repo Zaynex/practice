9dbdeaf009bfdb731e54046f03d2349be56e3687
zaynex@126.com <zaynex@126.com>
2017-01-09 21:18:39 +0800

    add

---
+++ work/react&redux/todolist/src/components/TodoList.js
@@ -0,0 +1,24 @@
+import React, { PropTypes } from 'react'
+import Todo from './Todo'
+
+TodoList.propTypes = {
+	todos: PropTypes.arrayOf(PropTypes.shape({
+		id: PropTypes.number.isRequired,
+		completed: PropTypes.bool.isRequired,
+		text: PropTypes.string.isRequired
+	}).isRequired).isRequired,
+	onTodoClick: PropTypes.func.isRequired
+}
+
+const TodoList = ({todos, onTodoClick}) => (
+	<ul>
+		{todos.map(todo => 
+			<Todo
+				key={todo.id}
+				{...todo}
+				onClick={() => onTodoClick(todo.id)}
+			/>
+		)}
+	</ul>
+)
+export default TodoList
