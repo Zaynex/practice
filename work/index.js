f2d6c5ea1b27e0836efbd212c94b2340b10a502d
zaynex@126.com <zaynex@126.com>
2017-01-20 20:05:05 +0800

    add promise

--- work/index.js
+++ work/index.js
@@ -252,3 +252,349 @@ Promise.race([
 		// foo被拒绝
 		//查看err了解哪种情况
 	})
+
+
+调用次数过多或者过少： 如果出现多次调用，Promise的定义方式使得它只被决议一次，会忽略后续任意调用。所以，then也 只会被调用一次。
+
+var p = {
+	then: function(cb, errcb) {
+		cb(42)
+		errcb('evil laugh')
+	}
+}
+p.then(
+	function fulfilled(val){
+		console.log(val)
+	},
+	function rejected(err) {
+		console.log(err)
+	}
+)
+
+var p = Promise.resolve(42)
+Promise.resolve(p)
+	.then(function fulfilled(val){
+		console.log(val)
+	}, function rejected(err){
+		console.log(err + '1111')
+	})
+
+
+链式流
+1. 不管对Promise调用then()都会创建并返回一个新的Promise
+2. 不管从then调用的完成回调返回值是什么，都会被自动设置为链接Promise的完成
+
+var p = Promise.resolve(21)
+var p2 = p.then(function(v){
+	console.log(v) // 21
+	return v * 2
+})
+
+p2.then(function(v){
+	console.log(v) // 42
+})
+
+
+var rejectedPr = new Promise(function(resolve, reject) {
+	resolve(Promise.reject('Oops'))
+})
+rejectedPr.then(
+	function fulfilled(){
+		//不会到这里
+		console.log('hh')
+	},
+	function rejected(err) {
+		console.log(err)
+	})
+
+
+try...catch 无法支持异步操作，还需要一些额外的环境支持，比如 生成器
+
+
+// err-fist式的回调风格
+function foo(cb) {
+	setTimeout(function(){
+		try{
+			var x = baz.bar()
+			cb(null, x)
+		}
+		catch(err) {
+			cb(err)
+		}
+	}, 100)
+}
+
+foo(function(err, val) {
+	if(err) {
+		console.log(err)
+	}else {
+		console.log(val)
+	}
+})
+
+//只有在baz.bar调用会同步立即成功或失败的情况下，try...catch才会生效
+
+// Promise 采用回调分离，一个回调用于处理完成情况，另外一个回调用于拒绝情况
+
+
+也有问题：
+var p = Promise.resolve(32)
+
+p.then(
+	function fulfilled(msg){
+		//因为数字没有 string函数，所以会抛出错误
+		console.log(msg.toLowerCase())
+	},
+	function rejected(err){
+		console.log(err)
+		//永远不会到这里
+	})
+
+但是这种错误在Promise是里无法获得通知的。因为是在fulfilled里面发生的。前面的Promise p 已经决议生成32了。
+
+这也是Promise错误处理易于出错的问题。
+
+所以一般会在底部加上catch来捕捉决议后fulfilled里产生的错误
+var p = Promise.resolve(32)
+
+p.then(
+	function fulfilled(msg){
+		//因为数字没有 string函数，所以会抛出错误
+		console.log(msg.toLowerCase())
+	},
+	function rejected(err){
+		console.log(err)
+		//永远不会到这里
+	})
+	.catch(handleErrors()
+
+function handleErrors(e){
+	console.log(e)
+}
+
+
+但是如果最后catch里面的函数也出错那该怎么办？
+
+这倒还真是个坑
+
+不过Promise有done
+var p = Promise.resolve(42)
+p.then(
+	function fulfilled(msg){
+		console.log(msg.toLowerCase())
+	})
+.done(null, handleErrors)
+// 如果handleErrors发生异常，会被全局抛出到这里
+done不会创建和返回Promise，如果done()的函数内部出现任何异常都会作为一个全局未处理函数抛出
+
+
+
+
+API介绍
+如果希望同时发送两个AJAX请求，不管它们以什么顺序全部完成之后再发送第三个AJAX请求
+使用 Promise.all
+var p1 = request('http://some.url.1')
+var p2 = request('http://some.url.2')
+Promise.all([p1, p2])
+	.then(function(msgs) {
+		//把p1和p2完成并将它们的消息传入
+		return request('http://some.url.3?v=' + msgs.join(','))
+	})
+	.then(function(msg){
+		console.log(msg)
+	})
+
+如果Promise.all返回的promise中任何一个成员被拒绝的话，主Promise.all会被立即拒绝
+
+Promise.race接受单个数组参数。也是由多个Promise,thenable或立即值组成，不过因为是竞态，立即值也没有意义。
+如果传一个空数组，Promise永远也不会决议。
+
+//取最先获得数据的那个api
+var p1 = request('http://some.url.1')
+var p2 = request('http://some.url.2')
+Promise.race([p1, p2])
+	.then(function(msg){
+		return request('http://some.url.3/?v=' + msg)
+	})
+	.then(function(msg){
+		console.log(msg)
+	})
+
+
+finally
+
+如果前面的回调函数里保存了一些要用的资源但因为某种情况出现了超时而被忽略，开发者认为需要一个finally()来注册回调函数，执行必要的清理工作。
+
+finally()会创建并返回一个新的Promise。
+
+
+//辅助工具
+if(!Promise.observe) {
+	Promise.observe = function(pr, cb){
+		//观察pr的决议
+		pr.then(
+			function fulfilled(msg){
+				//安排异步回调
+				Promise.resolve(msg).then(cb)
+			}, function rejected(err){
+				Promise.resolve(err).then(cb)
+			})
+	}
+	//返回最初的promise
+	return pr
+}
+
+
+Promise.race([
+	Promise.observe(
+		foo(), 
+		function cleanup(msg){
+			//在foo()之后清理，即使它没有在超时之前完成
+		}),
+	timeoutPromise(3000)	
+])
+
+if(Promise.first) {
+	Promise.first = function(prs) {
+		return new Promise(function(resolve, reject){
+			prs.forEach(function(pr){
+				Promise.resolve(pr)	
+				.then(resolve)
+			})
+		})
+	}
+}
+
+//接受一个数组的值（可以是Promise或者是其他值），外加要在每个值上运行一个函数（任务）作为参数
+//map()本身返回一个promise,其完成值是一个数组，该数组保存任务执行之后的异步完成值。
+if(!Promise.map) {
+	Promise.map = function(vals, cb){
+		return Promise.all(
+			vals.map(function(val) {
+				return new Promise(function(resolve){
+					cb(val, resolve)
+				})
+			}))
+	}
+}
+
+var p1 = Promise.resolve(32);
+var p2 = Promise.resolve(21);
+var p3 = Promise.resolve(20);
+
+Promise.map([p1, p2, p3], function(pr,done){
+	Promise.resolve(pr)
+		.then(
+			function(v){
+				done(v * 2);
+			}, done)
+})
+.then(function(vals){
+	console.log(vals)
+})
+
+var p1 = Promise.resolve(42);
+var p2 = Promise.resolve('hello world');
+var p3 = Promise.reject('Oops');
+
+Promise.race([p1, p2, p3])
+	.then(function(msg){
+		//哪个promise在前面，就先返回
+		console.log(msg) // 42
+	})
+
+Promise.all([p1, p2])
+	.then(function(msg){
+		console.log(msg) //[42, "hello world"]
+	})
+
+Promise.all([p1, p2, p3])
+	.catch(function(err){
+		console.log(err) // Oops
+	})
+
+注意;
+Promise.all()传入空数组时会立即完成，但Promise.race()会挂住，且永远不会决议。
+
+function getY(x){
+	return new Promise(function(resolve, reject){
+		setTimeout(function(){
+			resolve((3*x) -1)
+		}, 100)
+	})
+}
+
+
+function foo(bar,baz){
+	var x = bar * baz
+
+	return getY(x)
+			.then(function(y){
+				return [x, y]
+			})
+}
+
+foo(10, 20)
+	.then(function(msgs){
+		var x = msgs[0];
+		var y = msgs[1];
+
+		console.log(x, y); // 200 ,599
+})
+
+
+
+
+function getY(x){
+	return new Promise(function(resolve, reject){
+		setTimeout(function(){
+			resolve((3*x) -1)
+		}, 100)
+	})
+}
+function foo(bar, baz){
+	var x = bar * baz
+	return [
+		Promise.resolve(x),
+		getY(x)
+	];
+}
+//返回两个Promise
+Promise.all(foo(10, 20))
+	.then(function(msgs){
+		var x = msgs[0];
+		var y = msgs[1];
+
+		console.log(x, y)
+	})
+
+
+//ES6
+Promise.all(foo(10, 20))
+	.then(function[x, y]{
+		console.log(x, y)
+	})
+
+
+//相当于生成Promise的工厂
+if(!Promise.wrap){
+	Promise.wrap = function(fn){
+		return function(){
+			var args = [].slice.call(arguments);
+			return new Promise(function(resolve, reject){
+				fn.apply(null, args.concat(function(err, v){
+					if(err){
+						console.log(err)
+					}else {
+						resolve(v)
+					}
+				}))
+			})
+		}
+	}
+}
+
+var request = Promise.wrap(ajax)
+request('http:/some.url.1')
+	.then()
+	..
