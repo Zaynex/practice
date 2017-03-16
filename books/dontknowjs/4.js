817d809955dd924e805249a3a4ad0941e51df2c5
zaynex <zaynex@126.com>
2017-03-16 20:15:43 +0800

    隐式转换

--- books/dontknowjs/4.js
+++ books/dontknowjs/4.js
@@ -22,13 +22,34 @@ JSON.stringify(function(){}); // undefined
 /**
  * + 号为一元运算符时转换成数字相加，字符串加数组会生成NaN
  * 普通 加法会转成字符串拼接
- * @type {String}
+ * 
+ * 这样解释还不够规范
+ * 如果某个操作数是字符串或者能够通过以下步骤（toString）转换成字符串的话，+ 就能进行 拼接操作
+ * 如果其中一个操作数是对象，则首先对其调用ToPrimitive操作，再调用[DefaultValue] 操作
+ * * @type {String}
+ * 注意：  + "" 会先调用valueOf()方法，然后通过抽象操作toString()方法转为字符串
+ * 而String() 则是直接调用toString()
  */
 var a = "abcd";
 a + 5; // "abcd5"
 var b = "2333";
 var c = +b; // 2333
 var c = 5 + b; // '52333'
+var arr1 = [1, 2];
+var arr2 = [3,4];
+arr1 + arr2;// "1,23,4"
+
+var a = {
+	valueOf: () => 42,
+	toString: () => 4
+}
+a + ""; // "42"
+String(a); // "4"
+
+[] + {}; // "[object Object]"
+// {}.toString() 转换成"[object Object]", [].toString()为空字符串
+
+
 
 
 /**
@@ -47,4 +68,5 @@ if(~a.indexOf('lo')) {
 // if(a.indexOf('lo')>-1) {
 // 	// 匹配
 // }
+
 
